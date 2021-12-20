1e0638f5267731d5be005c7c632e119181a179bc
denokenya <dennismuteti25@gmail.com>
2021-12-20 15:56:55 +0300

    create controllers

---
+++ controllers/productController.js
@@ -0,0 +1,73 @@
+//import model
+import req from "express/lib/request";
+import res from "express/lib/response";
+import Product from "../models/Product";
+
+//get All products
+
+export const getProducts =async (req ,res) =>{
+    try{
+        const products =await Product.find();
+        res.json(products);
+
+    } catch (error) {
+        res.status(500).json({message :error.message});
+    }
+}
+
+//get single product
+
+export const getProductById =async (req ,res) =>{
+
+    try{
+        const product = await Product.findById(req.params.id);
+        res.json(product);
+
+    } catch (error) {
+        res.status(404).json({message: error.message});
+
+    }
+}
+
+//create a product
+
+export const saveProduct =async (req, res) =>{
+    const product = new Product(req.body);
+    try{
+        const savedProduct =await product.save();
+        res.status(201).json(saveProduct);
+
+    } catch (error){
+        res.status(400).json({message: error.message});
+    }
+}
+
+//Update a Product
+
+export const updateProduct =async (req,res) => {
+    const cekId = await Product.findById(req.params.id);
+    if(!cekId) return res.status(404).json({message: "No Data Found"});
+    try {
+        const updatedProduct =await Product.updateOne({_id: req.params.id}, {$set: req.body});
+        res.status(200).json(updatedProduct)
+
+    } catch (error){
+        res.status(400).json({message: error.message}):
+    }
+
+}
+
+//Delete a product
+
+export const deleteProduct = async (req,res) =>{
+    const cekId = await Product.findById(req.params.id);
+    if(!cekId) return res.status(404).json({message: "No Data Found"});
+    try{
+        const deletedProduct = await Product.deleteOne({_id: req.params.id});
+
+    } catch (error){
+        res.status(400).json({message :error.message});
+    }
+
+
+}
